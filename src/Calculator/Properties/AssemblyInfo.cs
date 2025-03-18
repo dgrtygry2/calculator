262bf962438bccc778d75ff930526e92ae2e142b
dgrtygry2 <aaronjo594@gmail.com>
2025-03-17 22:19:50 -0400

    Update branding

--- src/Calculator/Properties/AssemblyInfo.cs
+++ src/Calculator/Properties/AssemblyInfo.cs
@@ -4,12 +4,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Calculator")]
+[assembly: AssemblyTitle("BasinCalculator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft Corporation")]
-[assembly: AssemblyProduct("Microsoft Calculator")]
-[assembly: AssemblyCopyright("Copyright ©  Microsoft Corporation. All rights reserved.")]
+[assembly: AssemblyCompany("dgrtygry2")]
+[assembly: AssemblyProduct("dgrtygry2")]
+[assembly: AssemblyCopyright("Copyright ©  dgrtygry2 BasinCalculator.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
